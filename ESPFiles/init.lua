6667ce102653d3efa6062a299f1f231ce2db0f28
Nelis Willers <neliswillers@gmail.com>
2016-03-25 10:22:52 +0200

    removed lua files, added some pics

--- ESPFiles/init.lua
+++
@@ -1,8 +0,0 @@
-wifi.setmode(wifi.STATION)
-wifi.sta.config("YOURSSID","YOURWPA")
-wifi.sta.connect()
---wait a second for the connection to succeed
-tmr.delay(1000000)
-print(wifi.sta.status())
-print(wifi.sta.getip())
-print(wifi.sta.getmac())
